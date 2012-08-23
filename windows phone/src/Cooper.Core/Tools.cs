6e3559f373de7e6e81254a1c52c8c047022a0669
sunleepy <sunleepy@gmail.com>
2012-08-23 19:54:14 +0800

    add windows phone hybrid.

---
+++ windows phone/src/Cooper.Core/Tools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Microsoft.Phone.Controls;
+
+using System.Windows.Navigation;
+using Microsoft.Phone.Net.NetworkInformation;
+using System.Threading;
+using System.Windows;
+
+namespace Cooper.Core
+{
+    /// <summary>
+    /// 辅助工具类
+    /// </summary>
+    public class Tools
+    {
+        /// <summary>
+        /// 是否连接网络
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsOnline()
+        {
+            var interfaceList = new NetworkInterfaceList();
+            var interfaceInfo = interfaceList.First();
+
+            bool connected = interfaceInfo.InterfaceState == ConnectState.Connected;
+            return connected;
+        }
+    }
+}
