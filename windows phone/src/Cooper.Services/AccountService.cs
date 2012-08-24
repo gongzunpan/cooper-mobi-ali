61a5ca026328701ad6e1e5d0a2c3d2bbe853d649
sunleepy <sunleepy@gmail.com>
2012-08-24 19:44:40 +0800

    update windows phone

--- windows phone/src/Cooper.Services/AccountService.cs
+++ windows phone/src/Cooper.Services/AccountService.cs
@@ -10,10 +10,11 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Cooper.Core;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Cooper.Services
 {
-    public class AccountService
+    public class AccountService : WebRequestWrapper
     {
         /// <summary>
         /// Ark登录
@@ -21,25 +22,49 @@ namespace Cooper.Services
         /// <param name="domain"></param>
         /// <param name="username"></param>
         /// <param name="password"></param>
-        public string Login(string domain, string username, string password)
+        /// <param name="successCallback"></param>
+        /// <param name="failCallback"></param>
+        public void Login(string domain
+            , string username
+            , string password
+            , Action<string> successCallback
+            , Action<Exception> failCallback)
         {
-            var request = HttpWebRequest.CreateHttp(Constant.LOGIN_URL);
-            request.Method = "POST";
-            string resultString = string.Empty;
-            request.BeginGetResponse((IAsyncResult result) =>
-            {
-                var webRequest = result.AsyncState as HttpWebRequest;
-                var webResponse = (HttpWebResponse)webRequest.EndGetResponse(result);
-                using (Stream streamResult = webResponse.GetResponseStream())
-                {
-                    using (StreamReader reader = new StreamReader(streamResult))
-                    {
-                        //获取的返回值
-                        resultString = reader.ReadToEnd();
-                    }
-                }
-            }, request);
-            return resultString;
+            var dict = new Dictionary<string, string>();
+            dict.Add("cbDomain", domain);
+            dict.Add("tbLoginName", username);
+            dict.Add("tbPassword", password);
+            this.UploadString(Constant.LOGIN_URL, dict, successCallback, failCallback);
+
+            #region 过时
+            //var request = HttpWebRequest.CreateHttp(Constant.LOGIN_URL);
+            //request.Method = "POST";
+            //string resultString = string.Empty;
+            //request.BeginGetResponse((IAsyncResult result) =>
+            //{
+            //    var webRequest = result.AsyncState as HttpWebRequest;
+            //    var webResponse = (HttpWebResponse)webRequest.EndGetResponse(result);
+            //    using (Stream streamResult = webResponse.GetResponseStream())
+            //    {
+            //        using (StreamReader reader = new StreamReader(streamResult))
+            //        {
+            //            //获取的返回值
+            //            resultString = reader.ReadToEnd();
+            //        }
+            //    }
+            //}, request);
+            //return resultString;
+            #endregion
+        }
+        /// <summary>
+        /// 用户注销
+        /// </summary>
+        /// <param name="successCallback"></param>
+        /// <param name="failCallback"></param>
+        public void Logout(Action<string> successCallback
+            , Action<Exception> failCallback)
+        {
+            this.UploadString(Constant.LOGOUT_URL, new Dictionary<string, string>(), successCallback, failCallback);
         }
     }
 }
